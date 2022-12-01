d1dad9617d44138bb807ea5ed541f6c2acfdd682
Keila Lopez <keilalofra@gmail.com>
2022-11-30 22:44:42 -0600

    first and last commit

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  # We are able to get characters by Actor.first.characters
+
+  # Test: Actor.last.full_name
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  # Test: Actor.last.list_roles
+  def list_roles
+    self.characters.map do |character|
+        "#{character.name} - #{character.show.name}"
+    end
+  end
 end
