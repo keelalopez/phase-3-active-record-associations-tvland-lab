14b03d4b1c851138b5af508565e0d32b72945476
Keila Lopez <keilalofra@gmail.com>
2022-11-30 21:59:36 -0600

    commit tree 03a3dc942d32164f087907ca14c39b38aa8116d3

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  # Can access characters by Show.last.characters
+
+  # Test: 
+#   def network
+#     self.network_id
+#   end
+
 end
